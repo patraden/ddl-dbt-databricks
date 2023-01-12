6f79cdfe1e13679d13f7ff1df0ba32dcf17ea8b6
patraden <denis.patrakhin@gmail.com>
2023-01-12 12:10:46 +0100

    refactored pg_230 models with template

--- databricks/models/staging/pg_230/users/deals__pg_230__fbs_mt4_real2__mt4_users.sql
+++ databricks/models/staging/pg_230/users/deals__pg_230__fbs_mt4_real2__mt4_users.sql
@@ -1,64 +1,18 @@
-{{ config(
-        pre_hook="CREATE TABLE if not exists {{ source('dw', 'deals__pg_230__fbs_mt4_real2__mt4_users') }} USING parquet LOCATION 'gs://staging-databricks-{{ var('env') }}/pg_230__deals/fbs_mt4_real2__mt4_users/'",
-    ) }}
-with src as
-(
-select
- *,
- row_number() over (partition by login order by timestamp desc) rn
-from {{ source('dw', 'deals__pg_230__fbs_mt4_real2__mt4_users') }}
-)
-select
- cast(login as int) as login,
- cast(group as string) as group,
- cast(password as string) as password,
- cast(enable as int) as enable,
- cast(enable_change_password as int) as enable_change_password,
- cast(enable_read_only as int) as enable_read_only,
- cast(enable_otp as int) as enable_otp,
- cast(password_investor as string) as password_investor,
- cast(password_phone as string) as password_phone,
- cast(name as string) as name,
- cast(country as string) as country,
- cast(city as string) as city,
- cast(state as string) as state,
- cast(enable_change_pass as int) as enable_change_pass,
- cast(enable_readonly as int) as enable_readonly,
- cast(zipcode as string) as zipcode,
- cast(address as string) as address,
- cast(lead_source as string) as lead_source,
- cast(phone as string) as phone,
- cast(email as string) as email,
- cast(comment as string) as comment,
- cast(id as string) as id,
- cast(status as string) as status,
- cast(regdate as timestamp) as regdate,
- cast(lastdate as timestamp) as lastdate,
- cast(leverage as int) as leverage,
- cast(agent_account as int) as agent_account,
- cast(timestamp as timestamp) as timestamp,
- cast(last_ip as int) as last_ip,
- cast(balance as decimal(38,18)) as balance,
- cast(prevmonthbalance as decimal(38,18)) as prevmonthbalance,
- cast(prevbalance as decimal(38,18)) as prevbalance,
- cast(credit as decimal(38,18)) as credit,
- cast(interestrate as decimal(38,18)) as interestrate,
- cast(taxes as double) as taxes,
- cast(prevmonthequity as double) as prevmonthequity,
- cast(prevequity as decimal(38,18)) as prevequity,
- cast(otp_secret as string) as otp_secret,
- cast(send_reports as int) as send_reports,
- cast(mqid as long) as mqid,
- cast(user_color as long) as user_color,
- cast(equity as decimal(38,18)) as equity,
- cast(margin as decimal(38,18)) as margin,
- cast(margin_level as decimal(38,18)) as margin_level,
- cast(margin_free as decimal(38,18)) as margin_free,
- cast(conv_rates2 as decimal(38,18)) as conv_rates2,
- cast(modify_time as timestamp) as modify_time,
- cast(isarchive as boolean) as isarchive
-from src
-{% if is_incremental() %}
-  where rn = 1 and timestamp between '{{ var('data_interval_start') }}' and '{{ var('data_interval_end') }}'
-{% endif %}
+{% set src_table = 'deals__pg_230__fbs_mt4_real2__mt4_users' %}
+{% set dir = 'pg_230__deals/fbs_mt4_real2__mt4_users/' %}
 
+{{ config(
+        pre_hook=external_table_create_template(
+        src_schema='dw',
+        src_table=src_table,
+        env=var('env'),
+        dir=dir
+        )
+    ) }}
+{{ pg_230_mt4_users_model_template(
+    src_schema='dw',
+    src_table=src_table,
+   ) }}
+{% if is_incremental() %}
+ and timestamp between '{{ var('data_interval_start') }}' and '{{ var('data_interval_end') }}'
+{% endif %}
